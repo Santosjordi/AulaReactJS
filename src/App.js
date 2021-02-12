13c32f8cf46c44c499dfaf36d01c7dfa0d9fe3f5
Saint Jordi <jordiws@gmail.com>
2021-02-11 21:24:51 -0300

    concluído até os 26:06 https://www.youtube.com/watch?v=IOJoJGDowEY&t=793s

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import GithubImage from './github.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container text-center">
+        <h1 className="py-5 text-uppercase">GitHub Profile</h1>
+        <form>
+            <div className="form-group">
+                <div className="input-group">
+                    <input type="text" className="form-control" required/>
+                    <span className="input-group-btn">
+                        <button type="submit" className="btn btn-success">Search</button>
+                    </span>
+                </div>
+            </div>
+        </form>
+        <div className='py-5'>
+            <img src={GithubImage} className="responsive rounded-circle" alt="github user face" height="200px"/>
+        </div>
     </div>
   );
 }
